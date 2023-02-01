bfa6384ea2e4a11af51d623e8a01f93c70e1bd84
Renat Skosarev <skosarrrev@gmail.com>
2023-02-02 01:17:59 +0500

    Lesson 4 + Homework

---
+++ src/main/java/dev/skosarev/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package dev.skosarev.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Canon and Gigue in D Major";
+    }
+}
