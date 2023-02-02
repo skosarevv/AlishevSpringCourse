1485af1d635d29bb00f418ca49755b3018bc233f
Renat Skosarev <skosarrrev@gmail.com>
2023-02-02 18:30:59 +0500

    Lesson 6: Dependency Injection - Part 2

--- src/main/java/dev/skosarev/springcourse/MusicPlayer.java
+++ src/main/java/dev/skosarev/springcourse/MusicPlayer.java
@@ -3,8 +3,34 @@ package dev.skosarev.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {
+    }
+
     // IoC
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
