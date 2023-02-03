38b0fdc80cd73553bb172eba314c0e6464a34253
Renat Skosarev <skosarrrev@gmail.com>
2023-02-03 15:46:24 +0500

    Lesson 20: Homework done

--- src/main/java/dev/skosarev/springcourse/controllers/FirstController.java
+++ src/main/java/dev/skosarev/springcourse/controllers/FirstController.java
@@ -23,4 +23,46 @@ public class FirstController {
     public String goodByePage() {
         return "first/goodbye";
     }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") String a,
+                            @RequestParam("b") String b,
+                            @RequestParam("action") String action,
+                            Model model) {
+        try {
+            int a_num = Integer.parseInt(a);
+            int b_num = Integer.parseInt(b);
+
+            switch (action) {
+                case "multiplication":
+                case "m":
+                    model.addAttribute("result", "Result: " + (a_num * b_num));
+                    break;
+                case "addition":
+                case "a":
+                    model.addAttribute("result", "Result: " + (a_num + b_num));
+                    break;
+                case "subtraction":
+                case "s":
+                    model.addAttribute("result", "Result: " + (a_num - b_num));
+                    break;
+                case "division":
+                case "d":
+                    model.addAttribute("result", "Result: " + 1.0 * (a_num / b_num));
+                    break;
+                default:
+                    throw new UnsupportedOperationException();
+            }
+            System.out.println(a_num / b_num);
+        } catch (UnsupportedOperationException e) {
+            model.addAttribute("result", "Error: unsupported action (" + action + ')');
+        } catch (NumberFormatException e) {
+            model.addAttribute("result", "Error: values are not numbers");
+        } catch (ArithmeticException e) {
+            model.addAttribute("result", "Error: division by zero");
+        } catch (Exception e) {
+            model.addAttribute("result", "Unknown error");
+        }
+        return "first/calculator";
+    }
 }
