a8882deff6f0783a2041c8cfd25eb62a7a513cc4
Renat Skosarev <skosarrrev@gmail.com>
2023-02-03 14:37:22 +0500

    Lesson 20: Controller and View

--- src/main/java/dev/skosarev/springcourse/controllers/FirstController.java
+++ src/main/java/dev/skosarev/springcourse/controllers/FirstController.java
@@ -1,28 +1,26 @@
 package dev.skosarev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Goodbye, " + name + " " + surname);
+    public String goodByePage() {
         return "first/goodbye";
     }
 }
