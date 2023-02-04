fc628cef157c7e1068f9e39417add7e51f639268
Renat Skosarev <skosarrrev@gmail.com>
2023-02-04 12:57:51 +0500

    Lesson 21: CRUD, REST (Get), DAO

---
+++ src/main/java/dev/skosarev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package dev.skosarev.springcourse.dao;
+
+import dev.skosarev.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Renat"));
+        people.add(new Person(++PEOPLE_COUNT, "Anna"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
