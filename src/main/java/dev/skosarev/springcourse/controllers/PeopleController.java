7c908c9d895afc5822ce314fa5b860ef31cd0dc7
Renat Skosarev <skosarrrev@gmail.com>
2023-02-04 13:32:04 +0500

    Lesson 22: @ModelAttribute, HTML forms

--- src/main/java/dev/skosarev/springcourse/controllers/PeopleController.java
+++ src/main/java/dev/skosarev/springcourse/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package dev.skosarev.springcourse.controllers;
 
 import dev.skosarev.springcourse.dao.PersonDAO;
+import dev.skosarev.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -31,4 +30,14 @@ public class PeopleController {
         return "people/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 }
